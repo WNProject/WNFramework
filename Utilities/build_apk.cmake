2b0a620ea0489ffd60314e6c191cece9a4c034af
awoloszyn <awoloszyn@google.com>
2015-05-27 22:53:22 -0400

    Fixed build issue for android .apks on windows.
    If more than one process calls "android project update" at the same time
    an internal error occurs and causes it to crash, and the build
    steps to not complete. This change, on windows, forces all
    calls to android project update to be serialized. This effectively
    turns that part of the build into single-process, but only that piece
    of the build the rest can be scaled correctly.
    
    Change-Id: I41713e2fcc48e6bb821ee0f1dbc4f69a74ed6587
    Reviewed-on: https://wn-project.dnsalias.net/12
    Reviewed-by: devpow112 <devon.f.powell@gmail.com>
    Tested-by: devpow112 <devon.f.powell@gmail.com>

--- Utilities/build_apk.cmake
+++ Utilities/build_apk.cmake
@@ -55,7 +55,6 @@ function(build_apk)
   set(WN_ACTIVITY_NAME ${PARSED_ARGS_ACTIVITY})
   set(WN_PROGRAM_NAME ${PARSED_ARGS_PROGRAM_NAME})
   set(WN_TARGET_DIR ${CMAKE_CURRENT_BINARY_DIR}/${WN_PROGRAM_NAME})
-  get_target_property(SO_NAME ${PARSED_ARGS_TARGET} LOCATION)
   library_name(LIB_NAME ${PARSED_ARGS_TARGET})
 
   set(WN_NATIVE_LIBRARIES ${LIB_NAME})
@@ -68,7 +67,7 @@ function(build_apk)
     "${WN_TARGET_DIR}/src/${PARSED_ARGS_TOP_LEVEL}/${PARSED_ARGS_DOMAIN}/${PARSED_ARGS_SUBDOMAIN}/${WN_ACTIVITY_NAME}.java")
 
   set(WN_TARGET_NAME ${PARSED_ARGS_TARGET} )
-
+  
   add_custom_command(TARGET ${WN_TARGET_NAME}
     PRE_BUILD
     COMMAND ${CMAKE_COMMAND} -E remove_directory "${WN_TARGET_DIR}/libs/${ANDROID_ABI}")
@@ -77,20 +76,32 @@ function(build_apk)
     COMMAND ${CMAKE_COMMAND} -E make_directory "${WN_TARGET_DIR}/libs/${ANDROID_ABI}")
   add_custom_command(TARGET ${WN_TARGET_NAME}
     POST_BUILD
-    COMMAND ${CMAKE_COMMAND} -E copy "${SO_NAME}" "${WN_TARGET_DIR}/libs/${ANDROID_ABI}")
+    COMMAND ${CMAKE_COMMAND} -E copy "$<TARGET_FILE:${PARSED_ARGS_TARGET}>" "${WN_TARGET_DIR}/libs/${ANDROID_ABI}/"
+    DEPENDS $<TARGET_FILE:${PARSED_ARGS_TARGET}>)
 
   if (CMAKE_BUILD_TYPE STREQUAL "Debug")
     add_custom_command(TARGET ${WN_TARGET_NAME}
       POST_BUILD
-      COMMAND ${CMAKE_COMMAND} -E copy "${ANDROID_NDK}/prebuilt/android-${ANDROID_ARCH_NAME}/gdbserver"
+      COMMAND ${CMAKE_COMMAND} -E copy "${ANDROID_NDK}/prebuilt/android-${ANDROID_ARCH_NAME}/gdbserver/gdbserver"
         "${WN_TARGET_DIR}/libs/${ANDROID_ABI}/")
   endif()
-
+  
   if (ANDROID_SDK_DIR)
+    set(ANDROID_TOOL_PATH ${ANDROID_SDK_DIR}/tools/android)
+    if (${ANDROID_NDK_HOST_SYSTEM_NAME} STREQUAL "windows" OR
+      ${ANDROID_NDK_HOST_SYSTEM_NAME} STREQUAL "windows-x86_64")
+      find_host_program(PYTHON python REQUIRED)
+      set(ANDROID_TOOL_PATH ${PYTHON} ${WNFramework_SOURCE_DIR}/Utilities/single_process_windows.py
+        "WNFrameworkAndroidUpdateProject"
+        "${ANDROID_SDK_DIR}/tools/android.bat")
+    endif()
+
+# On windows we fail if we run more than once instance of android update at the same time.
     add_custom_command(TARGET ${WN_TARGET_NAME}
-      COMMAND ${ANDROID_SDK_DIR}/tools/android update project -t
+      POST_BUILD
+      COMMAND  ${ANDROID_TOOL_PATH} update project -t
         android-${ANDROID_NATIVE_API_LEVEL}
-        --name ${WN_TARGET_NAME} --path ${WN_TARGET_DIR} > ${WN_NULL_LOCATION}
+        --name ${WN_TARGET_NAME} --path "${WN_TARGET_DIR}" > ${WN_NULL_LOCATION}
         COMMENT "Creating android project for ${WN_TARGET_NAME}")
     find_host_program(ANT ant)
 
@@ -98,8 +109,9 @@ function(build_apk)
       message(WARNING "Cannot find ant, ANDROID APK will not get built")
     else()
       add_custom_command(TARGET ${WN_TARGET_NAME}
-        COMMAND ${ANT} ${WN_ANDROID_BUILD} > ${WN_NULL_LOCATION}
-        WORKING_DIRECTORY ${WN_TARGET_DIR}
+        POST_BUILD
+        COMMAND ${ANT} "${WN_ANDROID_BUILD}" > ${WN_NULL_LOCATION}
+        WORKING_DIRECTORY "${WN_TARGET_DIR}"
         COMMENT "Build android .apk for ${WN_TARGET_NAME}")
     endif()
   endif()
