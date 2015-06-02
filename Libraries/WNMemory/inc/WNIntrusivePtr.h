8c363d3b3b8c91c26a83d5c4424531e6a928f717
devpow112 <devon.f.powell@gmail.com>
2015-06-02 19:00:50 -0400

    Cleaned up intrusive_ptr
    Formatted to fit within new style guidelines
    Changed assignment operators to use the copy-and-swap idiom
    Added WN_NAMESPACE_BEGIN/WN_NAMESPACE_END macros to help with cleaning up of namespace declarations in files
    
    Change-Id: Ia9ebb62d46831b050b860e4de176e2ed68b418ca

--- Libraries/WNMemory/inc/WNIntrusivePtr.h
+++ Libraries/WNMemory/inc/WNIntrusivePtr.h
@@ -8,284 +8,293 @@
 #define __WN_MEMORY_INTRUSIVE_PTR_H__
 
 #include "WNMemory/inc/WNAllocation.h"
-
-#include <type_traits>
-
-namespace wn {
-    namespace memory {
-        template <typename _Type>
-        class intrusive_ptr final {
-        public:
-            typedef _Type element_type;
-
-            WN_FORCE_INLINE intrusive_ptr() :
-                m_pointer(wn_nullptr) {
-            }
-
-            WN_FORCE_INLINE explicit intrusive_ptr(const wn_nullptr_t) :
-                intrusive_ptr() {
-            }
-
-            WN_FORCE_INLINE explicit intrusive_ptr(element_type* _ptr, const wn_bool _add_reference = wn_true) :
-                intrusive_ptr() {
-                reset(_ptr, _add_reference);
-            }
-
-            template <typename _OtherType>
-            WN_FORCE_INLINE explicit intrusive_ptr(_OtherType* _ptr, const wn_bool _add_reference = wn_true) :
-                intrusive_ptr() {
-                reset(_ptr, _add_reference);
-            }
-
-            WN_FORCE_INLINE intrusive_ptr(intrusive_ptr&& _ptr) :
-                intrusive_ptr() {
-                swap(_ptr);
-            }
-
-            WN_FORCE_INLINE intrusive_ptr(const intrusive_ptr& _ptr) :
-                intrusive_ptr() {
-                reset(_ptr.get());
-            }
-
-            template <typename _OtherType>
-            WN_FORCE_INLINE intrusive_ptr(intrusive_ptr<_OtherType>&& _ptr) :
-                intrusive_ptr() {
-                reset(_ptr.get());
-
-                _ptr.reset();
-            }
-
-            template <typename _OtherType>
-            WN_FORCE_INLINE intrusive_ptr(const intrusive_ptr<_OtherType>& _ptr) :
-                intrusive_ptr() {
-                reset(_ptr.get());
-            }
-
-            WN_FORCE_INLINE ~intrusive_ptr() {
-                remove_reference();
-            }
-
-            WN_FORCE_INLINE element_type* operator -> () const {
-                return(get());
-            }
-
-            WN_FORCE_INLINE element_type& operator * () const {
-                return(*get());
-            }
-
-            WN_FORCE_INLINE operator wn_bool () const {
-                return(get() != wn_nullptr);
-            }
-
-            WN_FORCE_INLINE intrusive_ptr& operator = (const wn_nullptr_t) {
-                reset();
-
-                return(*this);
-            }
-
-            WN_FORCE_INLINE intrusive_ptr& operator = (intrusive_ptr&& _ptr) {
-                swap(_ptr);
-
-                return(*this);
-            }
-
-            WN_FORCE_INLINE intrusive_ptr& operator = (const intrusive_ptr& _ptr) {
-                reset(_ptr.get());
-
-                return(*this);
-            }
-
-            template <typename _OtherType>
-            WN_FORCE_INLINE intrusive_ptr& operator = (intrusive_ptr<_OtherType>&& _ptr) {
-                reset(_ptr.get());
-
-                _ptr.reset();
-
-                return(*this);
-            }
-
-            template <typename _OtherType>
-            WN_FORCE_INLINE intrusive_ptr& operator = (const intrusive_ptr<_OtherType>& _ptr) {
-                reset(_ptr.get());
-
-                return(*this);
-            }
-
-            WN_FORCE_INLINE element_type* get() const {
-                return(m_pointer);
-            }
-
-            WN_FORCE_INLINE wn_size_t use_count() const {
-                const element_type* pointer = get();
-
-                if (pointer != wn_nullptr) {
-                    return(pointer->reference_count());
-                }
-
-                return(0);
-            }
-
-            WN_FORCE_INLINE wn_bool unique() const {
-                return(use_count() == 1);
-            }
-
-            WN_FORCE_INLINE wn_void reset() {
-                remove_reference();
-            }
-
-            WN_FORCE_INLINE wn_void reset(wn_nullptr_t) {
-                reset();
-            }
-
-            WN_FORCE_INLINE wn_void reset(element_type* _ptr, const wn_bool _add_reference = wn_true) {
-                if (_ptr != wn_nullptr && _add_reference) {
-                    _ptr->add_reference();
-                }
-
-                remove_reference();
-
-                m_pointer = _ptr;
-            }
-
-            template <typename _OtherType>
-            WN_FORCE_INLINE wn_void reset(_OtherType* _ptr, const wn_bool _add_reference = wn_true) {
-                reset(static_cast<element_type*>(_ptr), _add_reference);
-            }
-
-            WN_FORCE_INLINE wn_void swap(intrusive_ptr& _ptr) {
-                const intrusive_ptr pointer(_ptr);
-
-                _ptr.reset(get());
-
-                reset(pointer.get());
-            }
-
-            WN_FORCE_INLINE element_type* release() {
-                element_type* pointer = get();
-
-                m_pointer = wn_nullptr;
-
-                return(pointer);
-            }
-
-        private:
-            template <typename _OtherType>
-            friend class intrusive_ptr;
-
-            WN_FORCE_INLINE wn_void add_reference() const {
-                if (m_pointer != wn_nullptr) {
-                    m_pointer->add_reference();
-                }
-            }
-
-            WN_FORCE_INLINE wn_void remove_reference() {
-                if (m_pointer != wn_nullptr) {
-                    if (m_pointer->remove_reference()) {
-                        destroy(m_pointer);
-                    }
-
-                    m_pointer = wn_nullptr;
-                }
-            }
-
-            element_type* m_pointer;
-        };
-
-        template <typename _Type1, typename _Type2>
-        WN_FORCE_INLINE wn_bool operator == (const intrusive_ptr<_Type1>& _lhs, const intrusive_ptr<_Type2>& _rhs) {
-            return(_lhs.get() == _rhs.get());
-        }
-
-        template <typename _Type1, typename _Type2>
-        WN_FORCE_INLINE wn_bool operator != (const intrusive_ptr<_Type1>& _lhs, const intrusive_ptr<_Type2>& _rhs) {
-            return(_lhs.get() != _rhs.get());
-        }
-
-        template <typename _Type1, typename _Type2>
-        WN_FORCE_INLINE wn_bool operator < (const intrusive_ptr<_Type1>& _lhs, const intrusive_ptr<_Type2>& _rhs) {
-            return(_lhs.get() < _rhs.get());
-        }
-
-        template <typename _Type1, typename _Type2>
-        WN_FORCE_INLINE wn_bool operator > (const intrusive_ptr<_Type1>& _lhs, const intrusive_ptr<_Type2>& _rhs) {
-            return(_lhs.get() > _rhs.get());
-        }
-
-        template <typename _Type1, typename _Type2>
-        WN_FORCE_INLINE wn_bool operator <= (const intrusive_ptr<_Type1>& _lhs, const intrusive_ptr<_Type2>& _rhs) {
-            return(_lhs.get() <= _rhs.get());
-        }
-
-        template <typename _Type1, typename _Type2>
-        WN_FORCE_INLINE wn_bool operator >= (const intrusive_ptr<_Type1>& _lhs, const intrusive_ptr<_Type2>& _rhs) {
-            return(_lhs.get() >= _rhs.get());
-        }
-
-        template <typename _Type>
-        WN_FORCE_INLINE wn_bool operator == (const intrusive_ptr<_Type>& _ptr, const wn_nullptr_t) {
-            return(_ptr.get() == wn_nullptr);
-        }
-
-        template <typename _Type>
-        WN_FORCE_INLINE wn_bool operator == (const wn_nullptr_t, const intrusive_ptr<_Type>& _ptr) {
-            return(wn_nullptr == _ptr.get());
-        }
-
-        template <typename _Type>
-        WN_FORCE_INLINE wn_bool operator != (const intrusive_ptr<_Type>& _ptr, const wn_nullptr_t) {
-            return(_ptr.get() != wn_nullptr);
-        }
-
-        template <typename _Type>
-        WN_FORCE_INLINE wn_bool operator != (const wn_nullptr_t, const intrusive_ptr<_Type>& _ptr) {
-            return(wn_nullptr != _ptr.get());
-        }
-
-        template <typename _Type>
-        WN_FORCE_INLINE wn_bool operator < (const intrusive_ptr<_Type>& _ptr, const wn_nullptr_t) {
-            return(_ptr.get() < wn_nullptr);
-        }
-
-        template <typename _Type>
-        WN_FORCE_INLINE wn_bool operator < (const wn_nullptr_t, const intrusive_ptr<_Type>& _ptr) {
-            return(wn_nullptr < _ptr.get());
-        }
-
-        template <typename _Type>
-        WN_FORCE_INLINE wn_bool operator >(const intrusive_ptr<_Type>& _ptr, const wn_nullptr_t) {
-            return(_ptr.get() > wn_nullptr);
-        }
-
-        template <typename _Type>
-        WN_FORCE_INLINE wn_bool operator > (const wn_nullptr_t, const intrusive_ptr<_Type>& _ptr) {
-            return(wn_nullptr > _ptr.get());
-        }
-
-        template <typename _Type>
-        WN_FORCE_INLINE wn_bool operator <= (const intrusive_ptr<_Type>& _ptr, const wn_nullptr_t) {
-            return(_ptr.get() <= wn_nullptr);
-        }
-
-        template <typename _Type>
-        WN_FORCE_INLINE wn_bool operator <= (const wn_nullptr_t, const intrusive_ptr<_Type>& _ptr) {
-            return(wn_nullptr <= _ptr.get());
-        }
-
-        template <typename _Type>
-        WN_FORCE_INLINE wn_bool operator >= (const intrusive_ptr<_Type>& _ptr, const wn_nullptr_t) {
-            return(_ptr.get() >= wn_nullptr);
-        }
-
-        template <typename _Type>
-        WN_FORCE_INLINE wn_bool operator >= (const wn_nullptr_t, const intrusive_ptr<_Type>& _ptr) {
-            return(wn_nullptr >= _ptr.get());
-        }
-
-        template <typename _Type, typename... _Args>
-        WN_FORCE_INLINE intrusive_ptr<_Type> make_intrusive(_Args&&... _args) {
-            return(intrusive_ptr<_Type>(construct<_Type>(std::forward<_Args>(_args)...)));
-        }
+#include "WNCore/inc/WNTypeTraits.h"
+
+WN_NAMESPACE_BEGIN(memory)
+
+template <typename T>
+class intrusive_ptr final {
+public:
+  typedef T element_type;
+
+  WN_FORCE_INLINE intrusive_ptr() :
+    m_pointer(wn_nullptr) {
+  }
+
+  WN_FORCE_INLINE intrusive_ptr(const wn_nullptr_t) :
+    intrusive_ptr() {
+  }
+
+  WN_FORCE_INLINE explicit intrusive_ptr(element_type* ptr,
+                                         const wn_bool add_ref = wn_true) :
+    intrusive_ptr() {
+    reset(ptr, add_ref);
+  }
+
+  template <typename Y>
+  WN_FORCE_INLINE explicit intrusive_ptr(Y* ptr,
+                                         const wn_bool add_ref = wn_true) :
+    intrusive_ptr() {
+    reset(ptr, add_ref);
+  }
+
+  WN_FORCE_INLINE intrusive_ptr(intrusive_ptr&& ptr) :
+    intrusive_ptr() {
+    swap(ptr);
+  }
+
+  WN_FORCE_INLINE intrusive_ptr(const intrusive_ptr& ptr) :
+    intrusive_ptr() {
+    reset(ptr.get());
+  }
+
+  template <typename Y>
+  WN_FORCE_INLINE intrusive_ptr(intrusive_ptr<Y>&& ptr) :
+    intrusive_ptr() {
+    reset(ptr.get());
+
+    ptr.reset();
+  }
+
+  template <typename Y>
+  WN_FORCE_INLINE intrusive_ptr(const intrusive_ptr<Y>& ptr) :
+    intrusive_ptr() {
+    reset(ptr.get());
+  }
+
+  WN_FORCE_INLINE ~intrusive_ptr() {
+    remove_reference();
+  }
+
+  WN_FORCE_INLINE element_type* operator -> () const {
+    return(get());
+  }
+
+  WN_FORCE_INLINE element_type& operator * () const {
+    return(*get());
+  }
+
+  WN_FORCE_INLINE operator wn_bool () const {
+    return(get() != wn_nullptr);
+  }
+
+  WN_FORCE_INLINE intrusive_ptr& operator = (const wn_nullptr_t) {
+    return(intrusive_ptr().swap(*this), *this);
+  }
+
+  WN_FORCE_INLINE intrusive_ptr& operator = (intrusive_ptr&& ptr) {
+    return(intrusive_ptr(std::move(ptr)).swap(*this), *this);
+  }
+
+  WN_FORCE_INLINE intrusive_ptr& operator = (const intrusive_ptr& ptr) {
+    return(intrusive_ptr(ptr).swap(*this), *this);
+  }
+
+  template <typename Y>
+  WN_FORCE_INLINE intrusive_ptr& operator = (intrusive_ptr<Y>&& ptr) {
+    return(intrusive_ptr(std::move(ptr)).swap(*this), *this);
+  }
+
+  template <typename Y>
+  WN_FORCE_INLINE intrusive_ptr& operator = (const intrusive_ptr<Y>& ptr) {
+    return(intrusive_ptr(ptr).swap(*this), *this);
+  }
+
+  WN_FORCE_INLINE element_type* get() const {
+    return(m_pointer);
+  }
+
+  WN_FORCE_INLINE wn_size_t use_count() const {
+    const element_type* pointer = get();
+
+    if (pointer != wn_nullptr) {
+      return(pointer->reference_count());
     }
+
+    return(0);
+  }
+
+  WN_FORCE_INLINE wn_bool unique() const {
+    return(use_count() == 1);
+  }
+
+  WN_FORCE_INLINE wn_void reset() {
+    remove_reference();
+  }
+
+  WN_FORCE_INLINE wn_void reset(const wn_nullptr_t) {
+    reset();
+  }
+
+  WN_FORCE_INLINE wn_void reset(element_type* ptr,
+                                const wn_bool add_ref = wn_true) {
+    if (ptr != wn_nullptr && add_ref) {
+      ptr->add_reference();
+    }
+
+    remove_reference();
+
+    m_pointer = ptr;
+  }
+
+  template <typename Y>
+  WN_FORCE_INLINE wn_void reset(Y* ptr,
+                                const wn_bool add_ref = wn_true) {
+    reset(static_cast<element_type*>(ptr), add_ref);
+  }
+
+  WN_FORCE_INLINE wn_void swap(intrusive_ptr& ptr) {
+    const intrusive_ptr pointer(ptr);
+
+    ptr.reset(get());
+
+    reset(pointer.get());
+  }
+
+  WN_FORCE_INLINE element_type* release() {
+    element_type* pointer = get();
+
+    m_pointer = wn_nullptr;
+
+    return(pointer);
+  }
+
+private:
+  template <typename Y>
+  friend class intrusive_ptr;
+
+  WN_FORCE_INLINE wn_void add_reference() const {
+    if (m_pointer != wn_nullptr) {
+      m_pointer->add_reference();
+    }
+  }
+
+  WN_FORCE_INLINE wn_void remove_reference() {
+    if (m_pointer != wn_nullptr) {
+      if (m_pointer->remove_reference()) {
+        destroy(m_pointer);
+      }
+
+      m_pointer = wn_nullptr;
+    }
+  }
+
+  element_type* m_pointer;
+};
+
+template <typename T, typename U>
+WN_FORCE_INLINE wn_bool operator == (const intrusive_ptr<T>& lhs,
+                                     const intrusive_ptr<U>& rhs) {
+  return(lhs.get() == rhs.get());
 }
+
+template <typename T, typename U>
+WN_FORCE_INLINE wn_bool operator != (const intrusive_ptr<T>& lhs,
+                                     const intrusive_ptr<U>& rhs) {
+  return(lhs.get() != rhs.get());
+}
+
+template <typename T, typename U>
+WN_FORCE_INLINE wn_bool operator < (const intrusive_ptr<T>& lhs,
+                                    const intrusive_ptr<U>& rhs) {
+  return(lhs.get() < rhs.get());
+}
+
+template <typename T, typename U>
+WN_FORCE_INLINE wn_bool operator > (const intrusive_ptr<T>& lhs,
+                                    const intrusive_ptr<U>& rhs) {
+  return(lhs.get() > rhs.get());
+}
+
+template <typename T, typename U>
+WN_FORCE_INLINE wn_bool operator <= (const intrusive_ptr<T>& lhs,
+                                     const intrusive_ptr<U>& rhs) {
+  return(lhs.get() <= rhs.get());
+}
+
+template <typename T, typename U>
+WN_FORCE_INLINE wn_bool operator >= (const intrusive_ptr<T>& lhs,
+                                     const intrusive_ptr<U>& rhs) {
+  return(lhs.get() >= rhs.get());
+}
+
+template <typename T>
+WN_FORCE_INLINE wn_bool operator == (const intrusive_ptr<T>& ptr,
+                                     const wn_nullptr_t) {
+  return(ptr.get() == wn_nullptr);
+}
+
+template <typename T>
+WN_FORCE_INLINE wn_bool operator == (const wn_nullptr_t,
+                                     const intrusive_ptr<T>& ptr) {
+  return(wn_nullptr == ptr.get());
+}
+
+template <typename T>
+WN_FORCE_INLINE wn_bool operator != (const intrusive_ptr<T>& ptr,
+                                     const wn_nullptr_t) {
+  return(ptr.get() != wn_nullptr);
+}
+
+template <typename T>
+WN_FORCE_INLINE wn_bool operator != (const wn_nullptr_t,
+                                     const intrusive_ptr<T>& ptr) {
+  return(wn_nullptr != ptr.get());
+}
+
+template <typename T>
+WN_FORCE_INLINE wn_bool operator < (const intrusive_ptr<T>& ptr,
+                                    const wn_nullptr_t) {
+  return(ptr.get() < wn_nullptr);
+}
+
+template <typename T>
+WN_FORCE_INLINE wn_bool operator < (const wn_nullptr_t,
+                                    const intrusive_ptr<T>& ptr) {
+  return(wn_nullptr < ptr.get());
+}
+
+template <typename T>
+WN_FORCE_INLINE wn_bool operator > (const intrusive_ptr<T>& ptr,
+                                    const wn_nullptr_t) {
+  return(ptr.get() > wn_nullptr);
+}
+
+template <typename T>
+WN_FORCE_INLINE wn_bool operator > (const wn_nullptr_t,
+                                    const intrusive_ptr<T>& ptr) {
+  return(wn_nullptr > ptr.get());
+}
+
+template <typename T>
+WN_FORCE_INLINE wn_bool operator <= (const intrusive_ptr<T>& ptr,
+                                     const wn_nullptr_t) {
+  return(ptr.get() <= wn_nullptr);
+}
+
+template <typename T>
+WN_FORCE_INLINE wn_bool operator <= (const wn_nullptr_t,
+                                     const intrusive_ptr<T>& ptr) {
+  return(wn_nullptr <= ptr.get());
+}
+
+template <typename T>
+WN_FORCE_INLINE wn_bool operator >= (const intrusive_ptr<T>& ptr,
+                                     const wn_nullptr_t) {
+  return(ptr.get() >= wn_nullptr);
+}
+
+template <typename T>
+WN_FORCE_INLINE wn_bool operator >= (const wn_nullptr_t,
+                                     const intrusive_ptr<T>& ptr) {
+  return(wn_nullptr >= ptr.get());
+}
+
+template <typename T, typename... Args>
+WN_FORCE_INLINE intrusive_ptr<T> make_intrusive(Args&&... args) {
+  return(intrusive_ptr<T>(construct<T>(std::forward<Args>(args)...)));
+}
+
+WN_NAMESPACE_END
 
 #endif // __WN_MEMORY_INTRUSIVE_PTR_H__
