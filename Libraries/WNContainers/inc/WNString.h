fe18ba70b6438bef0a5524f8f6468f3ab7f95241
devpow112 <devon.f.powell@gmail.com>
2015-07-25 11:51:06 -0400

    Added contiguous_range and string_view
    contiguous_range:
      Allows for a read/write view into a contiguous part of memory
      Has a number of the iteration concepts of containers
    
    string_view:
      Allows for a read-only view into a contiguous part of string data (string/char*/char[])
      Has a number of the iteration concepts of containers
      Has a number of the string read only concepts (find/rfind/find_first_of/find_last_of)
    
    Removed WNContainers overall header.  No need for this anymore.
    Added natvis for new types
    
    Change-Id: If881f39718cb29b3361953737521ae5f52cce570

---
+++ Libraries/WNContainers/inc/WNString.h
@@ -0,0 +1,23 @@
+// Copyright (c) 2014, WNProject Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#pragma once
+
+#ifndef __WN_CONTAINERS_STRING_H__
+#define __WN_CONTAINERS_STRING_H__
+
+#include "WNCore/inc/WNTypes.h"
+
+#include <string>
+#include <cstring>
+
+namespace wn {
+namespace containers {
+
+typedef std::string string;
+
+} // namespace containers
+} // namespace wn
+
+#endif // __WN_CONTAINERS_STRING_H__
--- Libraries/WNContainers/test/src/Tests/WNDynamicArrayTests.cpp
+++ Libraries/WNContainers/test/src/Tests/WNDynamicArrayTests.cpp
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 #include "WNContainers/test/inc/Common.h"
+#include "WNContainers/inc/WNDynamicArray.h"
 
 template <typename _Type>
 struct dynamic_array : ::testing::Test {};
