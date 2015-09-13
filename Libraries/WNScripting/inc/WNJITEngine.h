2f65c7028c2eb61c76f612e8e886389b26d0a8f1
Awoloszyn <awoloszyn@google.com>
2015-09-13 10:27:54 -0400

    Updated scripting engine with new node-types and cleaner parsing.
    A bunch of random cleamnups and reorganization.
    ASTLexer/ASTParser.{h,cpp} are all auto-generated.
    
    Change-Id: Ifff64efee3911eaf593286669b5e356f6ef197ce

--- Libraries/WNScripting/inc/WNJITEngine.h
+++ Libraries/WNScripting/inc/WNJITEngine.h
@@ -5,22 +5,30 @@
 #ifndef __WN_SCRIPTING_JIT_ENGINE_H__
 #define __WN_SCRIPTING_JIT_ENGINE_H__
 
+#include "WNLogging/inc/WNLog.h"
 #include "WNMemory/inc/WNAllocator.h"
 #include "WNScripting/inc/WNEngineFileManager.h"
 #include "WNScripting/inc/WNEngine.h"
+
+namespace WNLogging {
+class WNLogger;
+}
 
 namespace wn {
 namespace scripting {
 
 class jit_engine : public engine {
  public:
-  jit_engine(memory::allocator* _allocator, file_manager* _manager)
-      : m_allocator(_allocator), m_file_manager(_manager) {}
-
+  jit_engine(memory::allocator* _allocator, file_manager* _manager,
+             WNLogging::WNLog* _log)
+      : m_allocator(_allocator),
+        m_file_manager(_manager),
+        m_compilation_log(_log) {}
   parse_error parse_file(const char* file) override;
  private:
   memory::allocator* m_allocator;
   file_manager* m_file_manager;
+  WNLogging::WNLog* m_compilation_log;
 };
 }
 }
