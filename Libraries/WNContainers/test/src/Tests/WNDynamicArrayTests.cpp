5c6be12325102cab8005f6179a4f875a0bcbc42c
Awoloszyn <awoloszyn@google.com>
2015-09-13 09:59:19 -0400

    Added constructor for dynamic array to take a range of elements.
    Change-Id: I19448bf44cc77c5f3eeb5f8a95c01c32c54e645f

--- Libraries/WNContainers/test/src/Tests/WNDynamicArrayTests.cpp
+++ Libraries/WNContainers/test/src/Tests/WNDynamicArrayTests.cpp
@@ -8,6 +8,23 @@
 template <typename _Type>
 struct dynamic_array : ::testing::Test {};
 
-typedef ::testing::Types<wn_uint8, wn_uint16, wn_uint32, wn_uint64> dynamic_array_testing_types;
+typedef ::testing::Types<wn_uint8, wn_uint16, wn_uint32, wn_uint64>
+    dynamic_array_testing_types;
 
 TYPED_TEST_CASE(dynamic_array, dynamic_array_testing_types);
+
+TYPED_TEST(dynamic_array, range_construct) {
+  TypeParam buffer[5] = {TypeParam(0), TypeParam(1), TypeParam(2), TypeParam(3),
+                         TypeParam(4)};
+  wn::memory::default_test_allocator alloc;
+  {
+    wn::containers::dynamic_array<TypeParam> arr(buffer, buffer + 5, &alloc);
+
+    for (size_t i = 0; i < 5; ++i) {
+      EXPECT_EQ(TypeParam(i), arr[i]);
+      EXPECT_EQ(buffer[i], arr[i]);
+    }
+  }
+
+  EXPECT_EQ(alloc.freed(), alloc.allocated());
+}
