df627f9ce1e19a950d2d1df1094185052d71bc61
Awoloszyn <awoloszyn@google.com>
2015-09-13 10:35:00 -0400

    Added code-generation to JIT scripting engine.
    Change-Id: I27d4c03c8ba964d433e66f9ed3610bc26243c12d

--- Libraries/WNScripting/inc/WNJITEngine.h
+++ Libraries/WNScripting/inc/WNJITEngine.h
@@ -5,6 +5,8 @@
 #ifndef __WN_SCRIPTING_JIT_ENGINE_H__
 #define __WN_SCRIPTING_JIT_ENGINE_H__
 
+#include "WNContainers/inc/WNDeque.h"
+#include "WNContainers/inc/WNHashMap.h"
 #include "WNLogging/inc/WNLog.h"
 #include "WNMemory/inc/WNAllocator.h"
 #include "WNScripting/inc/WNEngineFileManager.h"
@@ -14,21 +16,38 @@ namespace WNLogging {
 class WNLogger;
 }
 
+namespace llvm {
+  class LLVMContext;
+  class EngineBuilder;
+  class ExecutionEngine;
+  class Module;
+}
+
 namespace wn {
 namespace scripting {
+
+struct CompiledModule {
+  std::unique_ptr<llvm::ExecutionEngine> m_engine;
+  llvm::Module* m_module;
+};
 
 class jit_engine : public engine {
  public:
   jit_engine(memory::allocator* _allocator, file_manager* _manager,
-             WNLogging::WNLog* _log)
-      : m_allocator(_allocator),
-        m_file_manager(_manager),
-        m_compilation_log(_log) {}
+             WNLogging::WNLog* _log);
+  ~jit_engine();
+  void_func get_function(containers::string_view _name) const override {
+    return m_pointers.find(_name)->second;
+  }
   parse_error parse_file(const char* file) override;
  private:
+  CompiledModule& add_module(containers::string_view _file);
   memory::allocator* m_allocator;
   file_manager* m_file_manager;
   WNLogging::WNLog* m_compilation_log;
+  std::unique_ptr<llvm::LLVMContext> m_context;
+  containers::deque<CompiledModule> m_modules;
+  containers::hash_map<containers::string_view, void(*)()> m_pointers;
 };
 }
 }
