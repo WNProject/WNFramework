13242b16b58a9d1cd852027328188cf8a9ce85a2
Awoloszyn <awoloszyn@google.com>
2015-09-13 10:35:00 -0400

    Added code-generation to JIT scripting engine.
    Change-Id: I27d4c03c8ba964d433e66f9ed3610bc26243c12d

--- Libraries/WNScripting/inc/WNEngine.h
+++ Libraries/WNScripting/inc/WNEngine.h
@@ -8,14 +8,17 @@
 #define __WN_SCRIPTING_ENGINE_H__
 
 #include "WNScripting/inc/WNErrors.h"
+#include "WNContainers/inc/WNStringView.h"
 
 namespace wn {
 namespace scripting {
 
 class engine {
 public:
+  typedef void(*void_func)();
   virtual ~engine() {}
   virtual parse_error parse_file(const char* file) = 0;
+  virtual void_func get_function(containers::string_view _name) const = 0;
 };
 
 }
