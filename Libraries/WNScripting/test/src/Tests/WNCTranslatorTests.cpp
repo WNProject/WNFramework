2ffc713834e9aebb2aa775799e995f3e1a58753a
Awoloszyn <awoloszyn@google.com>
2015-09-16 19:47:07 -0400

    Allowed scripts to contain multiple return statements.
    This would fail on LLVM builds.
    This now suppresses the lines in the AST so that we do not
    try and generate obviously unreachable code.
    
    Change-Id: I80493c274d65746469dc72d20e7faa3664243c44

--- Libraries/WNScripting/test/src/Tests/WNCTranslatorTests.cpp
+++ Libraries/WNScripting/test/src/Tests/WNCTranslatorTests.cpp
@@ -44,3 +44,22 @@ TEST(c_translator, multiple_c_functions) {
       "}\n",
       std::string(manager.get_file("file.wns.c")->data()));
 }
+
+// Make sure multiple returns get combined into just 1.
+TEST(c_translator, multiple_returns) {
+  wn::memory::default_expanding_allocator<50> allocator;
+  wn::scripting::test_file_manager manager(
+      &allocator, {{"file.wns", "Void main() { return; return; }"}});
+
+  scripting::c_translator translator(&allocator, &manager,
+                                     WNLogging::get_null_logger());
+  containers::string output_string(&allocator);
+  EXPECT_EQ(wn::scripting::parse_error::ok,
+            translator.translate_file("file.wns"));
+  EXPECT_EQ(
+      "void main() {\n"
+      "return;\n"
+      "}\n",
+      std::string(manager.get_file("file.wns.c")->data()));
+  
+}
