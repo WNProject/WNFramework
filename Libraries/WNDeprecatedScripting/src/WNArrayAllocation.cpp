0fdbd74e368345e065ef7965092ff8bbb7febe79
devpow112 <devon.f.powell@gmail.com>
2015-09-20 21:17:06 -0400

    Updated license information
    Changed LICENSE to LICENSE.txt
    Update all files to read copywrite 2015
    
    Change-Id: Ia0b16720b19c2541cb2b904a6b06210b16c42ba0

--- Libraries/WNDeprecatedScripting/src/WNArrayAllocation.cpp
+++ Libraries/WNDeprecatedScripting/src/WNArrayAllocation.cpp
@@ -1,6 +1,6 @@
-// Copyright (c) 2014, WNProject Authors. All rights reserved.
+// Copyright (c) 2015, WNProject Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+// found in the LICENSE.txt file.
 
 #include "WNDeprecatedScripting/inc/WNNodeHelpers.h" // This is intentionally out of order.
 // If this is included lower, it will fail because windows.h conflitcs wiht llvm.
