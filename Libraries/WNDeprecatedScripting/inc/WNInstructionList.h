93679a8651a35ba83c668eb8f5df3008391cff71
devpow112 <devon.f.powell@gmail.com>
2015-09-20 21:17:06 -0400

    Updated license information
    Changed LICENSE to LICENSE.txt
    Update all files to read copywrite 2015
    
    Change-Id: Ia0b16720b19c2541cb2b904a6b06210b16c42ba0
    Reviewed-on: https://wn-project.dnsalias.net/83
    Reviewed-by: awoloszyn <andrew.woloszyn@gmail.com>
    Tested-by: awoloszyn <andrew.woloszyn@gmail.com>

--- Libraries/WNDeprecatedScripting/inc/WNInstructionList.h
+++ Libraries/WNDeprecatedScripting/inc/WNInstructionList.h
@@ -1,6 +1,6 @@
-// Copyright (c) 2014, WNProject Authors. All rights reserved.
+// Copyright (c) 2015, WNProject Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+// found in the LICENSE.txt file.
 
 #ifndef __WN_INSTRUCTION_LIST_H__
 #define __WN_INSTRUCTION_LIST_H__
--- Libraries/WNLogging/inc/WNLog.inl
+++ Libraries/WNLogging/inc/WNLog.inl
@@ -1,6 +1,6 @@
-// Copyright (c) 2014, WNProject Authors. All rights reserved.
+// Copyright (c) 2015, WNProject Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+// found in the LICENSE.txt file.
 
 #ifndef __WN_LOG_INL__
 #define __WN_LOG_INL__
