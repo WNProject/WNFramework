7eaf3aca584558ad4969a61fa9baa97e86fc15a2
Awoloszyn <awoloszyn@google.com>
2016-06-21 21:04:27 -0400

    Added extra logging when compiled in debug mode.
    Specifically emit a message when we do not wait for the
    debugger.
    
    Change-Id: I2d187996f4c55efa9276dac36bb1f56717c9de7c
    Reviewed-on: https://wn-project.dnsalias.net/gerrit/262
    Try: Andrew Woloszyn <awoloszyn@google.com>
    Reviewed-by: Devon Powell <devon.f.powell@gmail.com>
    Tested-by: WNBot <wn.bot.1@gmail.com>

--- Overlays/Posix/Overlays/Android/Libraries/WNEntryPoint/src/WNEntry.cpp
+++ Overlays/Posix/Overlays/Android/Libraries/WNEntryPoint/src/WNEntry.cpp
@@ -94,6 +94,8 @@ void android_main(struct android_app* state) {
     __android_log_print(ANDROID_LOG_INFO, packageName, "--WAKING UP");
 
     fclose(debugFile);
+  } else {
+    __android_log_print(ANDROID_LOG_INFO, packageName, "--NOT_DEBUGGING");
   }
 #endif
 
