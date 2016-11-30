d79b6d94f2db4cfd8b48558af07bfd22df4ba8c8
devpow112 <devon.f.powell@gmail.com>
2016-11-29 20:18:09 -0500

    Removed multidimensional element array in WNMath
    Didn't make sense any more, want to do something different
    
    Change-Id: I6e4ea5985d4fc674a0692f54b67417cbb30fae67
    Reviewed-on: https://wn-project.dnsalias.net/gerrit/326
    Tested-by: WNBot <wn.bot.1@gmail.com>
    Try: Devon Powell <devon.f.powell@gmail.com>
    Reviewed-by: Andrew Woloszyn <awoloszyn@google.com>

--- Libraries/WNMath/inc/Internal/Generic/WNElementArray.h
+++ Libraries/WNMath/inc/Internal/Generic/WNElementArray.h
@@ -14,12 +14,12 @@ namespace wn {
 namespace math {
 namespace internal {
 
-template <typename T, const size_t Columns, const size_t Rows = 1>
+template <typename T, const size_t Dimension>
 class element_array_generic : core::non_copyable {
-  static_assert((Columns > 0) && (Rows > 0), "must have a count of at least 1");
+  static_assert(Dimension > 0, "must have a dimension of at least 1");
 
 public:
-  enum { count = (Columns * Rows), columns = Columns, rows = Rows };
+  enum { dimension = Dimension };
 
   element_array_generic() = default;
 
@@ -32,28 +32,11 @@ public:
   }
 
   WN_FORCE_INLINE T& at(const size_t _index) {
-    const size_t column = _index % Columns;
-    const size_t row = _index / Columns;
-
-    return at(column, row);
+    return m_values[_index];
   }
 
   WN_FORCE_INLINE const T& at(const size_t _index) const {
-    const size_t column = _index % Columns;
-    const size_t row = _index / Columns;
-
-    return at(column, row);
-  }
-
-  WN_FORCE_INLINE T& at(const size_t _column, const size_t _row) {
-    WN_DEBUG_ASSERT_DESC((_column < Columns) && (_row < Rows),
-        "attempting to get element outside of bounds");
-
-    return m_values[_column][_row];
-  }
-
-  WN_FORCE_INLINE const T& at(const size_t _column, const size_t _row) const {
-    return m_values[_column][_row];
+    return m_values[_index];
   }
 
   WN_FORCE_INLINE T* data() {
@@ -65,66 +48,7 @@ public:
   }
 
 private:
-  T m_values[Rows][Columns];
-};
-
-template <typename T, const size_t Count>
-class element_array_generic<T, Count, 1> : core::non_copyable {
-  static_assert(Count > 0, "must have a count of at least 1");
-
-public:
-  enum { count = Count, columns = Count, rows = 1 };
-
-  element_array_generic() = default;
-
-  WN_FORCE_INLINE T& operator[](const size_t _index) {
-    return at(_index);
-  }
-
-  WN_FORCE_INLINE const T& operator[](const size_t _index) const {
-    return at(_index);
-  }
-
-  WN_FORCE_INLINE T& at(const size_t _index) {
-    return at(_index, 0);
-  }
-
-  WN_FORCE_INLINE const T& at(const size_t _index) const {
-    return at(_index, 0);
-  }
-
-  WN_FORCE_INLINE T& at(const size_t _column, const size_t _row) {
-    WN_DEBUG_ASSERT_DESC((_column < Count) && (_row == 0),
-        "attempting to get element outside of bounds");
-
-#ifndef _WN_DEBUG
-    WN_UNUSED_ARGUMENT(_row);
-#endif
-
-    return m_values[_column];
-  }
-
-  WN_FORCE_INLINE const T& at(const size_t _column, const size_t _row) const {
-    WN_DEBUG_ASSERT_DESC((_column < Count) && (_row == 0),
-        "attempting to get element outside of bounds");
-
-#ifndef _WN_DEBUG
-    WN_UNUSED_ARGUMENT(_row);
-#endif
-
-    return m_values[_column];
-  }
-
-  WN_FORCE_INLINE T* data() {
-    return m_values;
-  }
-
-  WN_FORCE_INLINE const T* data() const {
-    return m_values;
-  }
-
-private:
-  T m_values[Count];
+  T m_values[Dimension];
 };
 
 }  // namespace internal
