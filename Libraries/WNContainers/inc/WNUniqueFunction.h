d22ac4bf55d29935797ede56683930a4c4c53661
devpow112 <devon.f.powell@gmail.com>
2016-11-30 17:24:44 -0500

    Added wn::containers::unique_function
    Allows for move only object storage since wn::containers::function requires copyable objects
    Added tests for wn::containers::unique_function
    Fixed bug in wn::containers::function copy code
    
    Change-Id: I8009267e22f16c269cadd3e103fd93c261104547
    Reviewed-on: https://wn-project.dnsalias.net/gerrit/307
    Try: Devon Powell <devon.f.powell@gmail.com>
    Tested-by: WNBot <wn.bot.1@gmail.com>
    Reviewed-by: Andrew Woloszyn <awoloszyn@google.com>

--- Libraries/WNContainers/inc/WNUniqueFunction.h
+++ Libraries/WNContainers/inc/WNUniqueFunction.h
@@ -4,8 +4,8 @@
 
 #pragma once
 
-#ifndef __WN_CONTAINERS_FUNCTION_H__
-#define __WN_CONTAINERS_FUNCTION_H__
+#ifndef __WN_CONTAINERS_UNIQUE_FUNCTION_H__
+#define __WN_CONTAINERS_UNIQUE_FUNCTION_H__
 
 #include "WNCore/inc/WNAssert.h"
 #include "WNCore/inc/WNUtility.h"
@@ -15,52 +15,40 @@ namespace wn {
 namespace containers {
 
 template <typename T>
-class function final {};
+class unique_function final {};
 
 template <typename R, typename... Args>
-class function<R(Args...)> {
+class unique_function<R(Args...)> {
 public:
   using result_type = R;
 
-  WN_FORCE_INLINE function()
-    : m_callable(nullptr),
-      m_executor(nullptr),
-      m_copier(nullptr),
-      m_deleter(nullptr) {}
+  WN_FORCE_INLINE unique_function()
+    : m_callable(nullptr), m_executor(nullptr), m_deleter(nullptr) {}
 
-  WN_FORCE_INLINE function(const nullptr_t) : function() {}
+  WN_FORCE_INLINE unique_function(const nullptr_t) : unique_function() {}
 
-  WN_FORCE_INLINE function(function&& _other) : function() {
+  WN_FORCE_INLINE unique_function(unique_function&& _other)
+    : unique_function() {
     swap(_other);
   }
 
-  WN_FORCE_INLINE function(const function& _other)
-    : m_callable(nullptr),
-      m_executor(_other.m_executor),
-      m_copier(_other.m_copier),
-      m_deleter(_other.m_deleter) {
-    if (m_copier) {
-      m_copier(&_other.m_callable, &m_callable);
-    }
-  }
-
   template <typename F,
-      typename =
-          core::enable_if_t<core::conjunction<core::is_callable<F, R(Args...)>,
-              core::negation<core::is_same_decayed<F, function>>>::value>>
-  WN_FORCE_INLINE function(F&& _f) : function() {
+      typename = core::enable_if_t<core::conjunction<
+          core::is_callable<F, R(Args...)>,
+          core::negation<core::is_same_decayed<F, unique_function>>>::value>>
+  WN_FORCE_INLINE unique_function(F&& _f) : unique_function() {
     construct(core::forward<F>(_f));
   }
 
   template <typename T>
-  WN_FORCE_INLINE function(R (T::*_f)(Args...), T* _obj)
-    : function(
+  WN_FORCE_INLINE unique_function(R (T::*_f)(Args...), T* _obj)
+    : unique_function(
           [_obj, _f](Args... _args) -> R { return ((_obj->*_f)(_args...)); }) {
     WN_RELEASE_ASSERT_DESC(_f, "member function is invalid");
     WN_RELEASE_ASSERT_DESC(_obj, "object instance is invalid");
   }
 
-  WN_FORCE_INLINE ~function() {
+  WN_FORCE_INLINE ~unique_function() {
     if (m_callable) {
       WN_DEBUG_ASSERT_DESC(m_deleter, "not in valid state to be cleaned up");
 
@@ -70,36 +58,30 @@ public:
     }
   }
 
-  WN_FORCE_INLINE function& operator=(const nullptr_t) {
-    function(nullptr).swap(*this);
+  WN_FORCE_INLINE unique_function& operator=(const nullptr_t) {
+    unique_function(nullptr).swap(*this);
 
     return *this;
   }
 
-  WN_FORCE_INLINE function& operator=(function&& _other) {
-    function(core::move(_other)).swap(*this);
-
-    return *this;
-  }
-
-  WN_FORCE_INLINE function& operator=(const function& _other) {
-    function(_other).swap(*this);
+  WN_FORCE_INLINE unique_function& operator=(unique_function&& _other) {
+    unique_function(core::move(_other)).swap(*this);
 
     return *this;
   }
 
   template <typename F,
-      typename =
-          core::enable_if_t<core::conjunction<core::is_callable<F, R(Args...)>,
-              core::negation<core::is_same_decayed<F, function>>>::value>>
-  WN_FORCE_INLINE function& operator=(F&& _f) {
-    function(core::forward<F>(_f)).swap(*this);
+      typename = core::enable_if_t<core::conjunction<
+          core::is_callable<F, R(Args...)>,
+          core::negation<core::is_same_decayed<F, unique_function>>>::value>>
+  WN_FORCE_INLINE unique_function& operator=(F&& _f) {
+    unique_function(core::forward<F>(_f)).swap(*this);
 
     return *this;
   }
 
   explicit WN_FORCE_INLINE operator bool() const {
-    return (m_callable && m_executor && m_deleter && m_copier);
+    return (m_callable && m_executor && m_deleter);
   }
 
   WN_FORCE_INLINE R operator()(Args... _args) const {
@@ -114,30 +96,28 @@ public:
   }
 
   template <typename F,
-      typename =
-          core::enable_if_t<core::conjunction<core::is_callable<F, R(Args...)>,
-              core::negation<core::is_same_decayed<F, function>>>::value>>
+      typename = core::enable_if_t<core::conjunction<
+          core::is_callable<F, R(Args...)>,
+          core::negation<core::is_same_decayed<F, unique_function>>>::value>>
   WN_FORCE_INLINE void assign(F&& _f) {
     *this = core::forward<F>(_f);
   }
 
   template <typename T>
   WN_FORCE_INLINE void assign(R (T::*_f)(Args...), T* _obj) {
-    function(_f, _obj).swap(*this);
+    unique_function(_f, _obj).swap(*this);
   }
 
-  WN_FORCE_INLINE void swap(function& _other) {
+  WN_FORCE_INLINE void swap(unique_function& _other) {
     if ((&_other) != this) {
       core::swap(m_callable, _other.m_callable);
       core::swap(m_executor, _other.m_executor);
-      core::swap(m_copier, _other.m_copier);
       core::swap(m_deleter, _other.m_deleter);
     }
   }
 
 private:
   using executor_type = R (*)(void* const*, Args...);
-  using copier_type = void (*)(void* const*, void**);
   using deleter_type = void (*)(void**);
 
   template <typename F>
@@ -156,15 +136,6 @@ private:
                   operator()(core::forward<Args>(_args)...));
     };
 
-    m_copier = [](void* const* _f, void** _ptr) -> void {
-      F* const p = *reinterpret_cast<F* const*>(_f);
-
-      (*_ptr) = memory::construct<F>(*p);
-
-      WN_RELEASE_ASSERT_DESC(
-          (*_ptr), "failed to construct lambda/function object");
-    };
-
     m_deleter = [](
         void** _f) -> void { memory::destroy<F>(reinterpret_cast<F*>(*_f)); };
   }
@@ -181,42 +152,39 @@ private:
           core::forward<Args>(_args)...));
     };
 
-    m_copier = [](void* const* _f, void** _ptr) -> void { (*_ptr) = (*_f); };
-
     m_deleter = [](void**) -> void {};
   }
 
   void* m_callable;
-  copier_type m_copier;
   deleter_type m_deleter;
   executor_type m_executor;
 };
 
 template <typename R, typename... Args>
 WN_FORCE_INLINE bool operator==(
-    const function<R(Args...)>& _f, const nullptr_t) {
+    const unique_function<R(Args...)>& _f, const nullptr_t) {
   return !_f;
 }
 
 template <typename R, typename... Args>
 WN_FORCE_INLINE bool operator==(
-    const nullptr_t, const function<R(Args...)>& _f) {
+    const nullptr_t, const unique_function<R(Args...)>& _f) {
   return (_f == nullptr);
 }
 
 template <typename R, typename... Args>
 WN_FORCE_INLINE bool operator!=(
-    const function<R(Args...)>& _f, const nullptr_t) {
+    const unique_function<R(Args...)>& _f, const nullptr_t) {
   return static_cast<bool>(_f);
 }
 
 template <typename R, typename... Args>
 WN_FORCE_INLINE bool operator!=(
-    const nullptr_t, const function<R(Args...)>& _f) {
+    const nullptr_t, const unique_function<R(Args...)>& _f) {
   return (_f != nullptr);
 }
 
 }  // namespace containers
 }  // namespace wn
 
-#endif  // __WN_CONTAINERS_FUNCTION_H__
+#endif  // __WN_CONTAINERS_UNIQUE_FUNCTION_H__
--- Libraries/WNContainers/test/src/Tests/WNUniqueFunctionTests.cpp
+++ Libraries/WNContainers/test/src/Tests/WNUniqueFunctionTests.cpp
@@ -3,6 +3,9 @@
 // found in the LICENSE.txt file.
 
 #include "WNContainers/inc/WNFunction.h"
+
+#include "WNContainers/inc/WNUniqueFunction.h"
+#include "WNMemory/inc/WNUniquePtr.h"
 #include "WNTesting/inc/WNTestHarness.h"
 
 void test_function1() {}
@@ -45,14 +48,14 @@ struct test_object final {
   int32_t m_value = 1;
 };
 
-TEST(function, construct) {
+TEST(unique_function, construct) {
   // default
-  const wn::containers::function<void()> function1;
-  const wn::containers::function<void(int32_t)> function2;
-  const wn::containers::function<void(int32_t, int32_t)> function3;
-  const wn::containers::function<int32_t()> function4;
-  const wn::containers::function<int32_t(int32_t)> function5;
-  const wn::containers::function<int32_t(int32_t, int32_t)> function6;
+  const wn::containers::unique_function<void()> function1;
+  const wn::containers::unique_function<void(int32_t)> function2;
+  const wn::containers::unique_function<void(int32_t, int32_t)> function3;
+  const wn::containers::unique_function<int32_t()> function4;
+  const wn::containers::unique_function<int32_t(int32_t)> function5;
+  const wn::containers::unique_function<int32_t(int32_t, int32_t)> function6;
 
   EXPECT_FALSE(function1);
   EXPECT_FALSE(function2);
@@ -62,16 +65,16 @@ TEST(function, construct) {
   EXPECT_FALSE(function6);
 
   // lambda
-  const wn::containers::function<void()> function7([]() -> void {});
-  const wn::containers::function<void(int32_t)> function8(
+  const wn::containers::unique_function<void()> function7([]() -> void {});
+  const wn::containers::unique_function<void(int32_t)> function8(
       [](int32_t) -> void {});
-  const wn::containers::function<void(int32_t, int32_t)> function9(
+  const wn::containers::unique_function<void(int32_t, int32_t)> function9(
       [](int32_t, int32_t) -> void {});
-  const wn::containers::function<int32_t()> function10(
+  const wn::containers::unique_function<int32_t()> function10(
       []() -> int32_t { return (1); });
-  const wn::containers::function<int32_t(int32_t)> function11(
+  const wn::containers::unique_function<int32_t(int32_t)> function11(
       [](int32_t) -> int32_t { return (1); });
-  const wn::containers::function<int32_t(int32_t, int32_t)> function12(
+  const wn::containers::unique_function<int32_t(int32_t, int32_t)> function12(
       [](int32_t, int32_t) -> int32_t { return (1); });
 
   EXPECT_TRUE(function7);
@@ -83,16 +86,17 @@ TEST(function, construct) {
 
   // lambda with capture
   const int32_t value = 1;
-  const wn::containers::function<void()> function13([value]() -> void {});
-  const wn::containers::function<void(int32_t)> function14(
+  const wn::containers::unique_function<void()> function13(
+      [value]() -> void {});
+  const wn::containers::unique_function<void(int32_t)> function14(
       [value](int32_t) -> void {});
-  const wn::containers::function<void(int32_t, int32_t)> function15(
+  const wn::containers::unique_function<void(int32_t, int32_t)> function15(
       [value](int32_t, int32_t) -> void {});
-  const wn::containers::function<int32_t()> function16(
+  const wn::containers::unique_function<int32_t()> function16(
       [value]() -> int32_t { return (1); });
-  const wn::containers::function<int32_t(int32_t)> function17(
+  const wn::containers::unique_function<int32_t(int32_t)> function17(
       [value](int32_t) -> int32_t { return (1); });
-  const wn::containers::function<int32_t(int32_t, int32_t)> function18(
+  const wn::containers::unique_function<int32_t(int32_t, int32_t)> function18(
       [value](int32_t, int32_t) -> int32_t { return (1); });
 
   EXPECT_TRUE(function13);
@@ -103,13 +107,15 @@ TEST(function, construct) {
   EXPECT_TRUE(function18);
 
   // functions
-  const wn::containers::function<void()> function19(&test_function1);
-  const wn::containers::function<void(int32_t)> function20(&test_function2);
-  const wn::containers::function<void(int32_t, int32_t)> function21(
+  const wn::containers::unique_function<void()> function19(&test_function1);
+  const wn::containers::unique_function<void(int32_t)> function20(
+      &test_function2);
+  const wn::containers::unique_function<void(int32_t, int32_t)> function21(
       &test_function3);
-  const wn::containers::function<int32_t()> function22(&test_function4);
-  const wn::containers::function<int32_t(int32_t)> function23(&test_function5);
-  const wn::containers::function<int32_t(int32_t, int32_t)> function24(
+  const wn::containers::unique_function<int32_t()> function22(&test_function4);
+  const wn::containers::unique_function<int32_t(int32_t)> function23(
+      &test_function5);
+  const wn::containers::unique_function<int32_t(int32_t, int32_t)> function24(
       &test_function6);
 
   EXPECT_TRUE(function19);
@@ -121,17 +127,17 @@ TEST(function, construct) {
 
   // member functions
   test_object object;
-  const wn::containers::function<void()> function25(
+  const wn::containers::unique_function<void()> function25(
       &test_object::test_member_function1, &object);
-  const wn::containers::function<void(int32_t)> function26(
+  const wn::containers::unique_function<void(int32_t)> function26(
       &test_object::test_member_function2, &object);
-  const wn::containers::function<void(int32_t, int32_t)> function27(
+  const wn::containers::unique_function<void(int32_t, int32_t)> function27(
       &test_object::test_member_function3, &object);
-  const wn::containers::function<int32_t()> function28(
+  const wn::containers::unique_function<int32_t()> function28(
       &test_object::test_member_function4, &object);
-  const wn::containers::function<int32_t(int32_t)> function29(
+  const wn::containers::unique_function<int32_t(int32_t)> function29(
       &test_object::test_member_function5, &object);
-  const wn::containers::function<int32_t(int32_t, int32_t)> function30(
+  const wn::containers::unique_function<int32_t(int32_t, int32_t)> function30(
       &test_object::test_member_function6, &object);
 
   EXPECT_TRUE(function25);
@@ -142,15 +148,15 @@ TEST(function, construct) {
   EXPECT_TRUE(function30);
 }
 
-TEST(function, move) {
+TEST(unique_function, move) {
   const int32_t value = 1;
-  wn::containers::function<int32_t()> function1(
+  wn::containers::unique_function<int32_t()> function1(
       [value]() -> int32_t { return (value); });
 
   EXPECT_TRUE(function1);
   EXPECT_EQ(function1(), 1);
 
-  wn::containers::function<int32_t()> function2(std::move(function1));
+  wn::containers::unique_function<int32_t()> function2(std::move(function1));
 
   EXPECT_FALSE(function1);
   EXPECT_TRUE(function2);
@@ -161,39 +167,24 @@ TEST(function, move) {
   EXPECT_TRUE(function1);
   EXPECT_FALSE(function2);
   EXPECT_EQ(function1(), 1);
+
+  wn::testing::allocator allocator;
+
+  {
+    wn::memory::unique_ptr<int32_t> int_ptr(
+        wn::memory::make_unique<int32_t>(&allocator, 5));
+    wn::containers::unique_function<int32_t()> function3(std::bind(
+        [&](wn::memory::unique_ptr<int32_t>& _ptr) -> int32_t {
+          return *(_ptr.get());
+        },
+        std::move(int_ptr)));
+
+    EXPECT_EQ(function3(), 5);
+  }
 }
 
-TEST(function, copy) {
-  const int32_t value = 1;
-  const wn::containers::function<int32_t()> function1(
-      [value]() -> int32_t { return (value); });
-
-  EXPECT_TRUE(function1);
-  EXPECT_EQ(function1(), 1);
-
-  const wn::containers::function<int32_t()> function2(function1);
-
-  EXPECT_TRUE(function1);
-  EXPECT_TRUE(function2);
-  EXPECT_EQ(function1(), 1);
-  EXPECT_EQ(function2(), 1);
-
-  wn::containers::function<int32_t()> function3;
-
-  EXPECT_FALSE(function3);
-
-  function3 = function2;
-
-  EXPECT_TRUE(function1);
-  EXPECT_TRUE(function2);
-  EXPECT_TRUE(function3);
-  EXPECT_EQ(function1(), 1);
-  EXPECT_EQ(function2(), 1);
-  EXPECT_EQ(function3(), 1);
-}
-
-TEST(function, assign) {
-  wn::containers::function<int32_t()> function1;
+TEST(unique_function, assign) {
+  wn::containers::unique_function<int32_t()> function1;
 
   EXPECT_FALSE(function1);
 
@@ -205,11 +196,11 @@ TEST(function, assign) {
   EXPECT_EQ(function1(), 1);
 }
 
-TEST(function, swap) {
+TEST(unique_function, swap) {
   const int32_t value = 1;
-  wn::containers::function<int32_t()> function1(
+  wn::containers::unique_function<int32_t()> function1(
       [value]() -> int32_t { return (value); });
-  wn::containers::function<int32_t()> function2(
+  wn::containers::unique_function<int32_t()> function2(
       [value]() -> int32_t { return (value + 1); });
 
   EXPECT_TRUE(function1);
@@ -224,7 +215,7 @@ TEST(function, swap) {
   EXPECT_EQ(function1(), 2);
   EXPECT_EQ(function2(), 1);
 
-  wn::containers::function<int32_t()> function3;
+  wn::containers::unique_function<int32_t()> function3;
 
   EXPECT_FALSE(function3);
 
@@ -235,9 +226,10 @@ TEST(function, swap) {
   EXPECT_EQ(function3(), 2);
 }
 
-TEST(function, large_parameter_count) {
-  wn::containers::function<int32_t(float, double, int8_t, int16_t, int32_t,
-      int64_t, uint8_t, uint16_t, uint32_t, uint64_t, size_t, signed_t)>
+TEST(unique_function, large_parameter_count) {
+  wn::containers::unique_function<int32_t(float, double, int8_t, int16_t,
+      int32_t, int64_t, uint8_t, uint16_t, uint32_t, uint64_t, size_t,
+      signed_t)>
       function1;
 
   EXPECT_FALSE(function1);
@@ -251,25 +243,4 @@ TEST(function, large_parameter_count) {
 
   EXPECT_TRUE(function1);
   EXPECT_EQ(function1(1.0f, 2.0, 3, 4, 5, 6, 7u, 8u, 9u, 10u, 11u, 12), -3);
-}
-
-struct foo {
-  foo(int _x) : x(_x) {}
-  foo(const foo&) = delete;
-  foo& operator=(const foo&) = delete;
-
-  foo(foo&& o) : x(o.x) {}
-  int32_t x;
-};
-
-using foo_func = wn::containers::function<int32_t(foo)>;
-
-TEST(function, non_copyable_parameter_test) {
-  auto f = foo_func([](foo f) { return f.x; });
-  EXPECT_EQ(5, f(foo(5)));
-
-  foo_func f2(f);
-  EXPECT_EQ(6, f2(foo(6)));
-
-  EXPECT_EQ(9, [&f]() { return f(foo(9)); }());
 }
