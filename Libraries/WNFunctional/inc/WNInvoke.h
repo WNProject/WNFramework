ae73c7fb0b0a958dfb3395c3fdf754f99785cee6
devpow112 <devon.f.powell@gmail.com>
2016-12-03 10:16:31 -0500

    Add invoke implementation
    Handles all standard requirements
    
    Change-Id: I8a0e0e9c8b924dba0a54ee3a411b505e725c16d4
    Reviewed-on: https://wn-project.dnsalias.net/gerrit/314
    Try: Devon Powell <devon.f.powell@gmail.com>
    Tested-by: WNBot <wn.bot.1@gmail.com>
    Reviewed-by: Andrew Woloszyn <awoloszyn@google.com>

---
+++ Libraries/WNFunctional/inc/WNInvoke.h
@@ -0,0 +1,103 @@
+#pragma once
+
+#ifndef __WN_FUNCTIONAL_INVOKE_H__
+#define __WN_FUNCTIONAL_INVOKE_H__
+
+#include "WNCore/inc/WNTypeTraits.h"
+#include "WNCore/inc/WNUtility.h"
+
+namespace wn {
+namespace functional {
+namespace internal {
+
+template <typename F, typename... Args>
+WN_FORCE_INLINE auto invoke(F&& _function, Args&&... _args)
+    -> core::enable_if_t<!core::is_member_pointer<core::decay_t<F>>::value,
+        decltype(core::forward<F>(_function)(core::forward<Args>(_args)...))> {
+  return (core::forward<F>(_function)(core::forward<Args>(_args)...));
+}
+
+template <typename Base, typename T, typename Derived, typename... Args>
+WN_FORCE_INLINE auto invoke(
+    T Base::*_member_function, Derived&& _reference, Args&&... _args)
+    -> core::enable_if_t<
+        core::is_function<T>::value &&
+            core::is_base_of<Base, core::decay_t<Derived>>::value,
+        decltype((core::forward<Derived>(_reference).*_member_function)(
+            core::forward<Args>(_args)...))> {
+  return ((core::forward<Derived>(_reference).*_member_function)(
+      core::forward<Args>(_args)...));
+}
+
+template <typename Base, typename T, typename Derived>
+WN_FORCE_INLINE auto invoke(T Base::*_member_data, Derived&& _reference)
+    -> core::enable_if_t<
+        !core::is_function<T>::value &&
+            core::is_base_of<Base, core::decay_t<Derived>>::value,
+        decltype(core::forward<Derived>(_reference).*_member_data)> {
+  return (core::forward<Derived>(_reference).*_member_data);
+}
+
+template <typename T>
+struct is_reference_wrapper : core::false_type {};
+
+template <typename U>
+struct is_reference_wrapper<std::reference_wrapper<U>> : core::true_type {};
+
+template <typename Base, typename T, typename ReferenceWrapper,
+    typename... Args>
+WN_FORCE_INLINE auto invoke(
+    T Base::*_member_function, ReferenceWrapper&& _reference, Args&&... _args)
+    -> core::enable_if_t<
+        core::is_function<T>::value &&
+            is_reference_wrapper<core::decay_t<ReferenceWrapper>>::value,
+        decltype((_reference.get().*_member_function)(
+            core::forward<Args>(_args)...))> {
+  return ((_reference.get().*_member_function)(core::forward<Args>(_args)...));
+}
+
+template <typename Base, typename T, typename Pointer, typename... Args>
+WN_FORCE_INLINE auto invoke(
+    T Base::*_member_function, Pointer&& ptr, Args&&... _args)
+    -> core::enable_if_t<
+        core::is_function<T>::value &&
+            !is_reference_wrapper<core::decay_t<Pointer>>::value &&
+            !core::is_base_of<Base, core::decay_t<Pointer>>::value,
+        decltype(((*core::forward<Pointer>(ptr)).*_member_function)(
+            core::forward<Args>(_args)...))> {
+  return (((*core::forward<Pointer>(ptr)).*_member_function)(
+      core::forward<Args>(_args)...));
+}
+
+template <typename Base, typename T, typename ReferenceWrapper>
+WN_FORCE_INLINE auto invoke(
+    T Base::*_member_data, ReferenceWrapper&& _reference)
+    -> core::enable_if_t<
+        !core::is_function<T>::value &&
+            is_reference_wrapper<core::decay_t<ReferenceWrapper>>::value,
+        decltype(_reference.get().*_member_data)> {
+  return (_reference.get().*_member_data);
+}
+
+template <typename Base, typename T, typename Pointer>
+WN_FORCE_INLINE auto invoke(T Base::*_member_data, Pointer&& ptr)
+    -> core::enable_if_t<
+        !core::is_function<T>::value &&
+            !is_reference_wrapper<core::decay_t<Pointer>>::value &&
+            !core::is_base_of<Base, core::decay_t<Pointer>>::value,
+        decltype((*core::forward<Pointer>(ptr)).*_member_data)> {
+  return ((*core::forward<Pointer>(ptr)).*_member_data);
+}
+
+}  // namespace internal
+
+template <typename F, typename... Args>
+WN_FORCE_INLINE auto invoke(F&& _f, Args&&... _args) -> decltype(
+    internal::invoke(core::forward<F>(_f), core::forward<Args>(_args)...)) {
+  return internal::invoke(core::forward<F>(_f), core::forward<Args>(_args)...);
+}
+
+}  // namespace functional
+}  // namespace wn
+
+#endif  //  __WN_FUNCTIONAL_INVOKE_H__
---
+++ Libraries/WNFunctional/test/src/WNInvokeTests.cpp
@@ -0,0 +1,102 @@
+// Copyright (c) 2016, WNProject Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE.txt file.
+
+#include "WNFunctional/inc/WNInvoke.h"
+#include "WNTesting/inc/WNTestHarness.h"
+
+// test helpers
+
+void no_return_no_parameters() {
+  // do nothing
+}
+
+int return_no_parameters() {
+  return 5;
+}
+
+int return_parameters(int _x) {
+  return _x;
+}
+
+struct object {
+  void no_return_no_parameters() {
+    // do nothing
+  }
+
+  int return_no_parameters() {
+    return 5;
+  }
+
+  int return_parameters(int _x) {
+    return _x;
+  }
+
+  int set_member(int _x) {
+    int t = m_y;
+
+    m_y = _x;
+
+    return t;
+  }
+
+  int get_member() {
+    return m_y;
+  }
+
+  int m_y = 1;
+};
+
+// tests
+
+TEST(invoke, static_functions) {
+  wn::functional::invoke(no_return_no_parameters);
+
+  EXPECT_EQ(wn::functional::invoke(return_no_parameters), 5);
+  EXPECT_EQ(wn::functional::invoke(return_parameters, 6), 6);
+}
+
+TEST(invoke, member_functions) {
+  object o;
+
+  wn::functional::invoke(&object::no_return_no_parameters, o);
+  wn::functional::invoke(&object::no_return_no_parameters, &o);
+
+  EXPECT_EQ(wn::functional::invoke(&object::return_no_parameters, o), 5);
+  EXPECT_EQ(wn::functional::invoke(&object::return_parameters, o, 6), 6);
+  EXPECT_EQ(wn::functional::invoke(&object::set_member, o, 2), 1);
+  EXPECT_EQ(wn::functional::invoke(&object::get_member, o), 2);
+  EXPECT_EQ(wn::functional::invoke(&object::return_no_parameters, &o), 5);
+  EXPECT_EQ(wn::functional::invoke(&object::return_parameters, &o, 6), 6);
+  EXPECT_EQ(wn::functional::invoke(&object::set_member, &o, 1), 2);
+  EXPECT_EQ(wn::functional::invoke(&object::get_member, &o), 1);
+}
+
+TEST(invoke, lambdas) {
+  auto lambda1 = []() {};
+  auto lambda2 = []() { return 5; };
+  auto lambda3 = [](int _x) { return _x; };
+
+  wn::functional::invoke(lambda1);
+
+  EXPECT_EQ(wn::functional::invoke(lambda2), 5);
+  EXPECT_EQ(wn::functional::invoke(lambda3, 6), 6);
+}
+
+TEST(invoke, functors) {
+  struct functor {
+    void operator()() {
+      // do nothing
+    }
+
+    int operator()(int _x) {
+      return _x;
+    }
+  };
+
+  functor f;
+
+  wn::functional::invoke(f);
+
+  EXPECT_EQ(wn::functional::invoke(f, 5), 5);
+}
