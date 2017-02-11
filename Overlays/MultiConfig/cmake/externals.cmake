1cdeb77cbee5f943c30d459b143700c42101c11c
devpow112 <devon.f.powell@gmail.com>
2017-02-11 15:57:25 -0500

    Fixed up externals
    Added googletest targets to Externals filter for MultiConfig overlay
    Fixed up naming of cmake function
    
    Change-Id: I78cce5821cdd57c434bac84349346d05f1bfeb4f
    Reviewed-on: https://wn-project.dnsalias.net/gerrit/336
    Try: Devon Powell <devon.f.powell@gmail.com>
    Reviewed-by: Andrew Woloszyn <awoloszyn@google.com>
    Tested-by: WNBot <wn.bot.1@gmail.com>

--- Overlays/MultiConfig/cmake/externals.cmake
+++ Overlays/MultiConfig/cmake/externals.cmake
@@ -1,4 +1,4 @@
-# Sets up the visual studio folder structure for gtest.
+# Sets up the visual studio folder structure for externals.
 foreach (target ${PARSED_ARGS_TARGETS})
     get_property(OLD_DIRECTORY TARGET ${target} PROPERTY FOLDER)
     set_property(TARGET ${target} PROPERTY FOLDER Externals/${name}/${OLD_DIRECTORY})
--- cmake/externals.cmake
+++ cmake/externals.cmake
@@ -1,4 +1,4 @@
-function(add_externals name)
+function(add_wn_externals name)
   cmake_parse_arguments(
     PARSED_ARGS
     ""
@@ -9,4 +9,3 @@ function(add_externals name)
   add_subdirectory(${PARSED_ARGS_DIRECTORY} ${name})
   overlay_named_file(cmake/externals.cmake)
 endfunction()
-
