8ef799f2bfe0ed876c0b9e11fcaf866297be965d
AWoloszyn <awoloszyn@google.com>
2017-08-22 21:14:46 -0400

    Fixed logging to always be  a "real" library.
    Change-Id: Id4cb78a12ce7ed08ad1400f968011e939edf18a1
    Reviewed-on: https://wn-project.dnsalias.net/gerrit/389
    Reviewed-by: Devon Powell <devon.f.powell@gmail.com>
    Try: Devon Powell <devon.f.powell@gmail.com>
    Tested-by: WNBot <wn.bot.1@gmail.com>

---
+++ Libraries/WNLogging/src/WNLog.cpp
@@ -0,0 +1,6 @@
+// Copyright (c) 2017, WNProject Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE.txt file.
+
+// This is intentionally empty.
+// This is a dummy file so that WNLogging is a valid library.
