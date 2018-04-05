eec4a78d67d3ee7e6cceabf7345a3fda472e59e2
devpow112 <devon.f.powell@gmail.com>
2018-04-05 10:58:08 -0400

    Return type optimization fixes
    This failed on some compilers due to it not being able to do RTO properly
     - clang 3.8.0
    
    Change-Id: I2ed65f454a7dc4f716791d6cf63d9fcef88aae5e
    Reviewed-on: https://wnproject.org/gerrit/203
    Try: devpow112 <devon.f.powell@gmail.com>
    Tested-by: WNBot <wn.bot.1@gmail.com>
    Reviewed-by: AWoloszyn <awoloszyn@google.com>

--- Libraries/WNScripting/src/conversion_helpers.cpp
+++ Libraries/WNScripting/src/conversion_helpers.cpp
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE.txt file.
 
+#include "WNCore/inc/WNUtility.h"
 #include "WNMemory/inc/WNUniquePtr.h"
 #include "WNScripting/inc/WNNodeTypes.h"
 #include "WNScripting/inc/internal/ast_convertor_context.h"
@@ -16,11 +17,14 @@ parse_ast_convertor::convertor_context::make_cast(
   if (from->m_type == _type) {
     return from;
   }
+
   auto ret = memory::make_unique<ast_cast_expression>(m_allocator, nullptr);
+
   ret->m_source_location = from->m_source_location;
   ret->m_base_expression = core::move(from);
   ret->m_type = _type;
-  return ret;
+
+  return core::move(ret);
 }
 
 memory::unique_ptr<ast_expression>
@@ -35,18 +39,22 @@ parse_ast_convertor::convertor_context::make_implicit_cast(
   }
 
   auto ret = memory::make_unique<ast_cast_expression>(m_allocator, nullptr);
+
   ret->m_source_location = from->m_source_location;
   ret->m_base_expression = core::move(from);
   ret->m_type = _type;
-  return ret;
+
+  return core::move(ret);
 }
 memory::unique_ptr<ast_statement>
 parse_ast_convertor::convertor_context::evaluate_expression(
     memory::unique_ptr<ast_expression> _expr) {
   auto ret = memory::make_unique<ast_evaluate_expression>(m_allocator, nullptr);
+
   ret->m_source_location = _expr->m_source_location;
   ret->m_expr = core::move(_expr);
-  return ret;
+
+  return core::move(ret);
 }
 
 memory::unique_ptr<ast_function_call_expression>
@@ -58,16 +66,22 @@ parse_ast_convertor::convertor_context::call_function(const node* _base_node,
       memory::make_unique<ast_function_call_expression>(
           m_allocator, _base_node);
   function_call->m_function = _func;
+
   WN_DEBUG_ASSERT(_expressions.size() == _func->m_parameters.size(),
       "Cannot create a function with an incorrect number of parameters");
+
   int i = 0;
   (void)i;
+
   for (auto& p : _expressions) {
     WN_DEBUG_ASSERT(_func->m_parameters[i++].m_type == p->m_type,
         "Trying to create function call with invalid type");
+
     function_call->initialized_parameters(m_allocator).push_back(core::move(p));
   }
+
   function_call->m_type = _func->m_return_type;
+
   return function_call;
 }
 
@@ -82,15 +96,18 @@ parse_ast_convertor::convertor_context::get_id(
       if (decl->m_name == _name) {
         id_expr->m_declaration = decl;
         id_expr->m_type = decl->m_type;
-        return id_expr;
+
+        return core::move(id_expr);
       }
     }
   }
+
   for (auto& parameter : m_current_function->m_parameters) {
     if (parameter.m_name == _name) {
       id_expr->m_function_parameter = &parameter;
       id_expr->m_type = parameter.m_type;
-      return id_expr;
+
+      return core::move(id_expr);
     }
   }
 
@@ -98,10 +115,12 @@ parse_ast_convertor::convertor_context::get_id(
     auto st =
         m_current_function->m_parameters[0].m_type->m_implicitly_contained_type;
     uint32_t num = 0;
+
     for (auto& m : st->m_structure_members) {
       if (m->m_name == _name) {
         auto mae = memory::make_unique<ast_member_access_expression>(
             m_allocator, _location);
+
         id_expr->m_function_parameter = &m_current_function->m_parameters[0];
         id_expr->m_type = m_current_function->m_parameters[0].m_type;
 
@@ -109,10 +128,13 @@ parse_ast_convertor::convertor_context::get_id(
         mae->m_member_offset = num;
         mae->m_member_name = core::move(_name);
         mae->m_type = m->m_type;
-        return mae;
+
+        return core::move(mae);
       }
+
       num++;
     }
+
     for (auto& f : st->m_member_functions) {
       if (f->m_name == _name) {
         auto mae = memory::make_unique<ast_member_access_expression>(
@@ -123,7 +145,8 @@ parse_ast_convertor::convertor_context::get_id(
         mae->m_base_expression = core::move(id_expr);
         mae->m_member_name = core::move(_name);
         mae->m_type = m_function_t.get();
-        return mae;
+
+        return core::move(mae);
       }
     }
   }
@@ -131,11 +154,13 @@ parse_ast_convertor::convertor_context::get_id(
   if (m_named_functions.find(_name) != m_named_functions.end()) {
     id_expr->m_function_name = core::move(_name);
     id_expr->m_type = m_function_t.get();
-    return id_expr;
+
+    return core::move(id_expr);
   }
 
   if (!_can_fail) {
     _location->log_line(m_log, logging::log_level::error);
+
     m_log->log_error("Could not find expression with name ", _name);
   }
 
