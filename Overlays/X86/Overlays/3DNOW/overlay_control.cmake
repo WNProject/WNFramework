b62b60dc795ef6419fc7d34b4080f2e437543448
devpow112 <devon.f.powell@gmail.com>
2018-04-18 16:44:35 -0400

    Removed unused stuff from x86 overlays
    Change-Id: Iee4eb4bc465b6d214810c35ce1ee708236d32d7b
    Reviewed-on: https://wnproject.org/gerrit/208
    Try: devpow112 <devon.f.powell@gmail.com>
    Tested-by: WNBot <wn.bot.1@gmail.com>
    Reviewed-by: AWoloszyn <awoloszyn@google.com>

--- Overlays/X86/Overlays/3DNOW/overlay_control.cmake
+++
@@ -1,7 +0,0 @@
-set(ENABLED OFF)
-option(WN_ENABLE_3DNOW "Enable 3DNow Support" ${WN_ENABLE_3DNOW})
-if (WN_ENABLE_3DNOW)
-  set(ENABLED ON)
-endif()
-
-register_overlay(${ENABLED} 3DNow)
--- Overlays/X86/Overlays/SSE/cmake/build_options.cmake
+++
@@ -1,1 +0,0 @@
-add_definitions(-D_WN_ENABLE_SSE)
