2a6cd49092162dfb45a71afda87bcc4d7e8b6f20
Devon Powell <devon.f.powell@gmail.com>
2020-10-25 21:39:59 -0400

    Include xxhash using its cmake files
    Supply XXHASH_BUILD_XXHSUM to disable including of xxhsum.

---
+++ Libraries/hashing/src/dummy.cpp
@@ -0,0 +1,7 @@
+// Copyright (c) 2020 WNProject Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE.txt file.
+
+// Intentionally left empty so that we can build a lib target for this library
+
+#include "hashing/inc/hash.h"
