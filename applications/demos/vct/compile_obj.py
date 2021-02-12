2adea9cc22ac49aef55b5af8aad9ce5026356810
WNBot <15212902+WNBot@users.noreply.github.com>
2021-02-12 14:14:47 +0000

    Update license year to 2021

--- applications/demos/vct/compile_obj.py
+++ applications/demos/vct/compile_obj.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# Copyright (c) 2018, WNProject Authors. All rights reserved.
+# Copyright (c) 2021, WNProject Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
