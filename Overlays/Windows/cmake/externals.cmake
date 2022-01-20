47e73caf17d7c45281c0cde104b7aa34ae9d490f
Devon Powell <devon.f.powell@gmail.com>
2022-01-20 08:40:38 -0500

    Fix windows builds (#412)

--- Overlays/Windows/cmake/externals.cmake
+++ Overlays/Windows/cmake/externals.cmake
@@ -1,4 +1,3 @@
-
 foreach(EXTERNAL_TARGET ${EXTERNAL_TARGETS})
     set_property(
       TARGET ${EXTERNAL_TARGET}
@@ -7,9 +6,14 @@ foreach(EXTERNAL_TARGET ${EXTERNAL_TARGETS})
 
     if (WN_USE_SCCACHE)
       get_target_property(CFLAGS ${EXTERNAL_TARGET} COMPILE_FLAGS)
-      string(REGEX REPLACE "/Zi" "" CFLAGS ${CFLAGS})
-      string(REGEX REPLACE "-Zi" "" CFLAGS ${CFLAGS})
-      set_target_properties(${EXTERNAL_TARGET} PROPERTIES 
-        COMPILE_FLAGS "${CFLAGS}")
+      if (COMPILE_FLAGS AND NOT COMPILE_FLAGS MATCHES ".*NOTFOUND")
+        string(REGEX REPLACE "/Zi" "" CFLAGS ${CFLAGS})
+        string(REGEX REPLACE "-Zi" "" CFLAGS ${CFLAGS})
+        set_target_properties(
+          ${EXTERNAL_TARGET}
+          PROPERTIES
+            COMPILE_FLAGS "${CFLAGS}"
+        )
+      endif()
     endif()
 endforeach()
